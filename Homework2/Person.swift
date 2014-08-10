edb2a7dcdd332db2d93da1d0a129d3e145cf7eb1
brmendez <brmendez@gmail.com>
2014-08-09 19:34:18 -0700

    Added Person.swift

---
+++ Homework2/Person.swift
@@ -0,0 +1,33 @@
+//
+//  Person.swift
+//  Homework2
+//
+//  Created by Brian Mendez on 8/9/14.
+//  Copyright (c) 2014 Brian Mendez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Person {
+    
+    var firstName : String
+    var lastName : String
+    
+    
+    var image : UIImage?
+    var hasImage = false
+    
+    
+    init(firstName : String, lastName : String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        
+    }
+    
+    func fullName() -> String {
+        return firstName + " " + lastName
+    }
+    
+}
+
